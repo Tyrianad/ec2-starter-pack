bef07159a15319d44de64b0e01c1b4454a99b047
Tyrianad <orio.f87@gmail.com>
2019-10-27 22:41:04 +0100

    Initial commit

---
+++ create-laravel-project.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+PROJECTS_FOLDER='projects/'
+USER_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+ROOT_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+
+if [ -z "$1" ]
+  then
+	echo -e "${RED}Please provide a folder name${NC}"
+else
+  echo "" &&
+  echo -e "${GREEN}----------------------------" &&
+  echo "1.CREATING FOLDERS" &&
+  echo -e "----------------------------${NC}" &&
+  echo "" &&
+
+	mkdir $PROJECTS_FOLDER$1 &&
+	cp -r defaults/. $PROJECTS_FOLDER$1 &&
+	cd $PROJECTS_FOLDER$1 &&
+
+	echo "" &&
+  echo -e "${GREEN}----------------------------" &&
+  echo "2.GENERATING USER AND ROOT PASSWORD" &&
+  echo -e "----------------------------${NC}" &&
+  echo "" &&
+
+	sed -i 's/{dbpassword}/'$USER_PASS'/g' .env &&
+  sed -i 's/{dbpassword}/'$USER_PASS'/g' docker-compose.yml &&
+	sed -i 's/{rootpassword}/'$ROOT_PASS'/g' docker-compose.yml &&
+
+	echo "" &&
+  echo -e "${GREEN}----------------------------" &&
+  echo "3.COMPOSING LARAVEL PROJECT" &&
+  echo -e "----------------------------${NC}" &&
+  echo "" &&
+
+  mkdir site &&
+  cd site &&
+	composer create-project --prefer-dist laravel/laravel . &&
+	cp ./.env .env &&
+
+	echo "" &&
+  echo -e "${GREEN}----------------------------" &&
+  echo "4.ADDING IDE_HELPER" &&
+  echo -e "----------------------------${NC}" &&
+  echo "" &&
+
+	composer require --dev barryvdh/laravel-ide-helper
+
+	echo "" &&
+  echo -e "${GREEN}----------------------------" &&
+  echo "DONE" &&
+  echo -e "----------------------------${NC}" &&
+  echo ""
+
+fi
---
+++ first-run.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo "" &&
+echo -e "${GREEN}----------------------------" &&
+echo "1.CREATING FOLDER" &&
+echo -e "----------------------------${NC}" &&
+echo "" &&
+
+if ! [ -d "projects" ]; then
+  mkdir -p projects
+fi
+
+echo "" &&
+echo -e "${GREEN}----------------------------" &&
+echo "2.UPDATING AND REMOVING OLD VERSIONS" &&
+echo -e "----------------------------${NC}" &&
+echo "" &&
+
+sudo apt-get remove docker docker-engine docker.io &&
+sudo apt-get update &&
+sudo apt-get upgrade -y &&
+
+echo "" &&
+echo -e "${GREEN}----------------------------" &&
+echo "3a.INSTALLING UTILITIES,NPM" &&
+echo -e "----------------------------${NC}" &&
+echo "" &&
+
+sudo apt-get install zip unzip php-zip php-mbstring php-xml -y &&
+sudo apt-get install npm -y &&
+
+echo "" &&
+echo -e "${GREEN}----------------------------" &&
+echo "3b.INSTALLING AND CONFIGURING DOCKER" &&
+echo -e "----------------------------${NC}" &&
+echo "" &&
+
+sudo apt-get install docker.io docker-compose -y &&
+sudo systemctl start docker &&
+sudo systemctl enable docker &&
+sudo usermod -aG docker $USER
+
+echo "" &&
+echo -e "${GREEN}----------------------------" &&
+echo "4.CREATING SWAP" &&
+echo -e "----------------------------${NC}" &&
+echo "" &&
+
+sudo fallocate -l 1G /swapfile &&
+sudo chmod 600 /swapfile &&
+sudo mkswap /swapfile &&
+sudo swapon /swapfile &&
+sudo cp /etc/fstab /etc/fstab.bak &&
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab &&
+
+echo "" &&
+echo -e "${GREEN}----------------------------" &&
+echo "5.INSTALLING COMPOSER" &&
+echo -e "----------------------------${NC}" &&
+echo "" &&
+
+sudo apt-get install composer -y
+
+echo "" &&
+echo -e "${GREEN}----------------------------" &&
+echo "6.CLEANING" &&
+echo -e "----------------------------${NC}" &&
+echo "" &&
+
+sudo apt-get autoclean -y
+sudo apt-get autoremove -y
+
+echo "" &&
+echo -e "${GREEN}----------------------------" &&
+echo "DONE" &&
+echo -e "----------------------------${NC}" &&
+echo ""
